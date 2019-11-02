9a5ca7cdcb325f9fcb11eee8ffd51123913e673a
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-11-03 00:54:54 +0100

    Adding tabs with correct sizes and colors | problem with tabbarview's swipe animation

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,22 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  int indexPage;
+
+  @override
+  void initState() {
+    this.indexPage = 1;
+    this._tabController = TabController(vsync: this, length: 4, initialIndex: this.indexPage);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -11,30 +27,62 @@ class App extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           bottom: TabBar(
+            controller: this._tabController,
             isScrollable: true,
             indicatorColor: Colors.white,
             indicatorWeight: 2.5,
+            onTap: (active) {
+              this.setState(() {
+                this.indexPage = active;
+              });
+            },
             tabs: [
               Container(
-                child: Icon(Icons.camera_alt),
+                child: Icon(
+                  Icons.camera_alt,
+                  color: (this.indexPage == 0) ? Colors.white : Colors.white70,
+                ),
                 width: 20.0,
               ),
               Container(
-                child: Center(child: Text("CHATS", style: TextStyle(fontWeight: FontWeight.w700),),),
+                child: Center(
+                  child: Text(
+                    "CHATS",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: (this.indexPage == 1) ? Colors.white : Colors.white70,
+                    ),
+                  ),
+                ),
                 width: tabWidth,
-                padding: const EdgeInsets.symmetric(vertical: 13.0),
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
               ),
               Container(
-                child: Center(child: Text("STATUS", style: TextStyle(fontWeight: FontWeight.w700),),),
+                child: Center(
+                  child: Text(
+                    "STATUS",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: (this.indexPage == 2) ? Colors.white : Colors.white70,
+                    ),
+                  ),
+                ),
                 width: tabWidth,
-                padding: const EdgeInsets.symmetric(vertical: 13.0),
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
               ),
               Container(
-                child: Center(child: Text("CALLS", style: TextStyle(fontWeight: FontWeight.w700),),),
+                child: Center(
+                  child: Text(
+                    "CALLS",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      color: (this.indexPage == 3) ? Colors.white : Colors.white70,
+                    ),
+                  ),
+                ),
                 width: tabWidth,
-                padding: const EdgeInsets.symmetric(vertical: 13.0),
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
               ),
-              
             ],
           ),
           title: Container(
@@ -44,11 +92,28 @@ class App extends StatelessWidget {
           ),
         ),
         body: TabBarView(
+          controller: _tabController,
           children: [
-            Container(),
-            Icon(Icons.directions_car),
-            Icon(Icons.directions_transit),
-            Icon(Icons.directions_bike),
+            Container(
+              color: Colors.red,
+              width: size.width,
+              height: size.height,
+            ),
+            Container(
+              color: Colors.green,
+              width: size.width,
+              height: size.height,
+            ),
+            Container(
+              color: Colors.yellow,
+              width: size.width,
+              height: size.height,
+            ),
+            Container(
+              color: Colors.blue,
+              width: size.width,
+              height: size.height,
+            ),
           ],
         ),
       ),
