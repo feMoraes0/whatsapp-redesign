7fae4d9ac27fe9db7031914a1c13ad6e7e3dcbea
Fernando Moraes <fernandomoraes.lopes@gmail.com>
2019-12-16 18:54:37 +0100

    Restarting project

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
-# whatsapp_clone
+<h1 align="center">Whatsapp Redesign</h1>
 
-A new Flutter project.
+## Prints
+<!-- <p align="center">
+  <img width="200" src="print.project.screen"/>
+</p> -->
 
-## Getting Started
+## How to use
 
-This project is a starting point for a Flutter application.
+To clone and run this application, you'll need [Git](https://git-scm.com/downloads) and [Flutter](https://flutter.dev/docs/get-started/install) installed on your computer. From your command line:
 
-A few resources to get you started if this is your first Flutter project:
+```
+# Clone this repository
+$ git clone https://github.com/feMoraes0/whatsapp-redesign.git
 
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+# Go into the repository
+$ cd whatsapp-redesign
 
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+# Install dependencies
+$ flutter packages get
+
+# Run the app
+$ flutter run
+```
+
+## Credits
+  - <a target="_blank" href="https://dribbble.com/shots/6802745-Whatsapp-Redesign">Design</a>
+
+## Contact
+  - <a target="_blank" href="https://www.linkedin.com/in/fernando-moraes-48a26916a/">LinkedIn</a>
+  - <a target="_blank" href="mailto:fernandomoraes.lopes@gmail.com">E-mail</a>
--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/app/screens/chats/chats.dart';
-import 'package:whatsapp_clone/app/screens/status/status.dart';
 
 class App extends StatefulWidget {
   @override
@@ -8,149 +6,14 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> with SingleTickerProviderStateMixin {
-  TabController _tabController;
-  int indexPage;
 
   @override
   void initState() {
-    this.indexPage = 1;
-    this._tabController = TabController(
-      vsync: this,
-      length: 4,
-      initialIndex: this.indexPage,
-    );
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double tabWidth = (size.width / 3) - 50.0;
-
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: TabBar(
-            controller: this._tabController,
-            isScrollable: true,
-            indicatorColor: Colors.white,
-            indicatorWeight: 2.5,
-            onTap: (active) {
-              this.setState(() {
-                this.indexPage = active;
-              });
-            },
-            tabs: [
-              Container(
-                child: Icon(
-                  Icons.camera_alt,
-                  color: (this.indexPage == 0) ? Colors.white : Colors.white70,
-                ),
-                width: 20.0,
-              ),
-              Container(
-                child: Center(
-                  child: Text(
-                    "CHATS",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      color:
-                          (this.indexPage == 1) ? Colors.white : Colors.white70,
-                    ),
-                  ),
-                ),
-                width: tabWidth,
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-              ),
-              Container(
-                child: Center(
-                  child: Text(
-                    "STATUS",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      color:
-                          (this.indexPage == 2) ? Colors.white : Colors.white70,
-                    ),
-                  ),
-                ),
-                width: tabWidth,
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-              ),
-              Container(
-                child: Center(
-                  child: Text(
-                    "CALLS",
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      color:
-                          (this.indexPage == 3) ? Colors.white : Colors.white70,
-                    ),
-                  ),
-                ),
-                width: tabWidth,
-                padding: const EdgeInsets.symmetric(vertical: 10.0),
-              ),
-            ],
-          ),
-          title: Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "WhatsApp",
-                  style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.w700),
-                ),
-                Row(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 3.0),
-                      child: Icon(
-                        Icons.search,
-                        size: 26.0,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 2.0),
-                      child: Icon(
-                        Icons.more_vert,
-                        size: 26.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: [
-            Container(
-              color: Colors.red,
-              width: size.width,
-              height: size.height,
-            ),
-            Chats(),
-            Status(),
-            Container(
-              color: Colors.blue,
-              width: size.width,
-              height: size.height,
-            ),
-          ],
-        ),
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.green[400],
-          onPressed: () {
-            print("floating button");
-          },
-          child: Icon(
-            Icons.message,
-            size: 24.0,
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
